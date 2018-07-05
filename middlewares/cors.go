542feba89f8a059ed47751971df09acc26597648
qjpcpu <qjpcpu@gmail.com>
2018-07-05 11:22:47 +0800

    opt CORS

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -11,18 +11,15 @@ func CorsHandle() gin.HandlerFunc {
 	devMode := conf.Get().DevMode
 	return func(c *gin.Context) {
 		from := c.Request.Header.Get("Origin")
-		if from != "" {
-			if strings.Contains(from, conf.Get().Domain) || devMode {
-				// ADD CODE HERE: 跨域处理
-				c.Writer.Header().Set("Access-Control-Allow-Origin", from)
-				c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if devMode || strings.Contains(from, conf.Get().Domain) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", from)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
-				c.Writer.Header().Set("Access-Control-Allow-Headers", "content-type, X-Requested-With")
-				c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT,PATCH")
-				if c.Request.Method == "OPTIONS" {
-					c.AbortWithStatus(http.StatusOK)
-				}
-			}
+			c.Writer.Header().Set("Access-Control-Allow-Headers", c.Request.Header.Get("Access-Control-Request-Headers"))
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT,PATCH")
+		}
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
 		}
 	}
 }
